9ab22b4dfe5b91a2d959ae489ac46f62a748dc61
Abhay Rajan <abhay78@gmail.com>
2024-01-05 14:01:20 -0600

    Add get_lines

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-# Function that parses command line args
-# Function that reads bytes
-
 import sys
 import getopt   
 import os
 
 def main(argv):
-    opts, args = getopt.getopt(argv, "-c:")
+    opts, args = getopt.getopt(argv, "c:l:")
 
     for opt, arg in opts:
-        if opt == '-c':
-            get_bytes(arg)
+        match opt:
+            case '-c':
+                get_bytes(arg)        
+            case '-l':
+                get_lines(arg)
 
 def get_bytes(file_name):
     print(os.path.getsize(file_name),file_name)
+
+def get_lines(file_name):
+    num_lines = 0
+    with open(file_name) as file_handler:
+        for line in file_handler:
+            num_lines += 1
+    print(num_lines, file_name)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
